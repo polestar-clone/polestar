7d993e108b240e91fa86b0a39c32e6d8a5cc7dc8
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2020-09-02 23:34:50 -0300

    Updates

---
+++ precache-manifest.eb01d9c38b0b02305e180301516163b5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085b66f18c46a18319919b35625da863",
+    "url": "/polestar/index.html"
+  },
+  {
+    "revision": "be866a36d6961d72d576",
+    "url": "/polestar/static/css/main.c6264bea.chunk.css"
+  },
+  {
+    "revision": "c41fdc7a2be8a72d89cd",
+    "url": "/polestar/static/js/2.d1d2d758.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/polestar/static/js/2.d1d2d758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be866a36d6961d72d576",
+    "url": "/polestar/static/js/main.e713a981.chunk.js"
+  },
+  {
+    "revision": "9878b9566039b95e9958",
+    "url": "/polestar/static/js/runtime-main.c3a32179.js"
+  },
+  {
+    "revision": "b0ca121c3914a545b3601f377f5ac39e",
+    "url": "/polestar/static/media/logo.b0ca121c.webp"
+  },
+  {
+    "revision": "d403204f8a6c1874bbb1ca186b992aa0",
+    "url": "/polestar/static/media/pic1-mobile.d403204f.webp"
+  },
+  {
+    "revision": "7a884debed339eab4ca1a4da9b4edc6b",
+    "url": "/polestar/static/media/pic1-mobileJPG.7a884deb.jpg"
+  },
+  {
+    "revision": "c9fcd4e6da0d81046402b3f5e8e612de",
+    "url": "/polestar/static/media/pic1.c9fcd4e6.webp"
+  },
+  {
+    "revision": "dcb2560a11a5dc77aa63cb3a4314c9c4",
+    "url": "/polestar/static/media/pic1JPG.dcb2560a.jpg"
+  },
+  {
+    "revision": "a681ccde22bca425f02f0df91d73fec4",
+    "url": "/polestar/static/media/pic2.a681ccde.webp"
+  },
+  {
+    "revision": "35dce972a699c8f864833a6c76c5a5f5",
+    "url": "/polestar/static/media/pic2JPG.35dce972.jpg"
+  },
+  {
+    "revision": "c6e08552cbe2129b580ae36225c878a8",
+    "url": "/polestar/static/media/ps1-configurator-menu-1.c6e08552.webp"
+  },
+  {
+    "revision": "b87f90c5925e47f63bd757c4633c2caa",
+    "url": "/polestar/static/media/ps1-configurator-menu-jpg-1.b87f90c5.jpg"
+  },
+  {
+    "revision": "7d34294df17f2dc42f9079c559c6f292",
+    "url": "/polestar/static/media/ps1-explore-menu-1.7d34294d.webp"
+  },
+  {
+    "revision": "daed78c2e873f43040e9006169899bb1",
+    "url": "/polestar/static/media/ps1-explore-menu-jpg-1.daed78c2.jpg"
+  },
+  {
+    "revision": "7d0a4989d20cbfaa2b3438eeaae6b568",
+    "url": "/polestar/static/media/ps1-full-jpg.7d0a4989.jpg"
+  },
+  {
+    "revision": "443d51df9823b52267d66f50fb7732ce",
+    "url": "/polestar/static/media/ps1-full.443d51df.webp"
+  },
+  {
+    "revision": "d85ee67ed2b3c7e1c4c98ad8f78bff0a",
+    "url": "/polestar/static/media/ps1-half-jpg.d85ee67e.jpg"
+  },
+  {
+    "revision": "168a641b6c46e3c15aad37a3b41aebd4",
+    "url": "/polestar/static/media/ps1-half.168a641b.webp"
+  },
+  {
+    "revision": "d3ad06425092964d4d8af4c6449248e6",
+    "url": "/polestar/static/media/ps2-configurator-menu-1.d3ad0642.webp"
+  },
+  {
+    "revision": "7fb7bb2cecd51baaede80f3098d06d72",
+    "url": "/polestar/static/media/ps2-configurator-menu-jpg-1.7fb7bb2c.jpg"
+  },
+  {
+    "revision": "457a880521d0b20b18c019b23e4de3cd",
+    "url": "/polestar/static/media/ps2-explore-menu-1.457a8805.webp"
+  },
+  {
+    "revision": "50db851c9363b45af437bee2e2549e05",
+    "url": "/polestar/static/media/ps2-explore-menu-jpg-1.50db851c.jpg"
+  },
+  {
+    "revision": "76bdb9c630f375ae78da8072d2ecf320",
+    "url": "/polestar/static/media/ps2-full-jpg.76bdb9c6.jpg"
+  },
+  {
+    "revision": "8c54b2574fbc6747446b3acd94bd1f45",
+    "url": "/polestar/static/media/ps2-full.8c54b257.webp"
+  },
+  {
+    "revision": "76b088201cfed7e55591f2072682f63d",
+    "url": "/polestar/static/media/ps2-half-jpg.76b08820.jpg"
+  },
+  {
+    "revision": "680adbbf6f9c422560bb315523318aae",
+    "url": "/polestar/static/media/ps2-half.680adbbf.webp"
+  }
+]);
